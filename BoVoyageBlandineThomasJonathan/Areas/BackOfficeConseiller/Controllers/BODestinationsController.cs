8813bd6a4fc5615c7415f7471dcf995c43ee43e8
Jonathan Bourdon <jonathbourdon@gmail.com>
2018-07-26 15:45:33 +0200

    Rajout controllers BO

---
+++ BoVoyageBlandineThomasJonathan/Areas/BackOfficeConseiller/Controllers/BODestinationsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoVoyageBlandineThomasJonathan.Data;
+using BoVoyageBlandineThomasJonathan.Models;
+
+namespace BoVoyageBlandineThomasJonathan.Areas.BackOfficeConseiller.Controllers
+{
+    public class BODestinationsController : Controller
+    {
+        private BoVoyageBTJDbContext db = new BoVoyageBTJDbContext();
+
+        // GET: BackOfficeConseiller/BODestinations
+        public ActionResult Index()
+        {
+            return View(db.Destinations.ToList());
+        }
+
+        // GET: BackOfficeConseiller/BODestinations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Destination destination = db.Destinations.Find(id);
+            if (destination == null)
+            {
+                return HttpNotFound();
+            }
+            return View(destination);
+        }
+
+        // GET: BackOfficeConseiller/BODestinations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BackOfficeConseiller/BODestinations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Continent,Pays,Region,Description")] Destination destination)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Destinations.Add(destination);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(destination);
+        }
+
+        // GET: BackOfficeConseiller/BODestinations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Destination destination = db.Destinations.Find(id);
+            if (destination == null)
+            {
+                return HttpNotFound();
+            }
+            return View(destination);
+        }
+
+        // POST: BackOfficeConseiller/BODestinations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Continent,Pays,Region,Description")] Destination destination)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(destination).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(destination);
+        }
+
+        // GET: BackOfficeConseiller/BODestinations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Destination destination = db.Destinations.Find(id);
+            if (destination == null)
+            {
+                return HttpNotFound();
+            }
+            return View(destination);
+        }
+
+        // POST: BackOfficeConseiller/BODestinations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Destination destination = db.Destinations.Find(id);
+            db.Destinations.Remove(destination);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
