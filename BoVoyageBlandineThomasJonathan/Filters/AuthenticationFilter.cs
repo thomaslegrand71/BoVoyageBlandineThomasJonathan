e863ccbc127bfadfbddad205379485ddfeb3c3ab
Jonathan Bourdon <jonathbourdon@gmail.com>
2018-07-27 16:45:01 +0200

    Ajout de l'authentification

---
+++ BoVoyageBlandineThomasJonathan/Filters/AuthenticationFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageBlandineThomasJonathan.Filters
+{
+    public class AuthenticationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["USER_BO"] == null)
+            {
+                filterContext.Result = new RedirectResult("\\BackOfficeConseiller\\Authentication\\Login");
+            }
+        }
+    }
+}
