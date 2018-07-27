37ada4246a58e00bd6ef1eb357e42a55ca9bd4c7
Blandine <blandine.ygou@live.com>
2018-07-27 21:02:21 +0200

    Renommage form DossierResa

--- BoVoyageBlandineThomasJonathan/Controllers/DossierReservationsController.cs
+++ BoVoyageBlandineThomasJonathan/Controllers/DossierReservationsController.cs
@@ -58,7 +58,7 @@ namespace BoVoyageBlandineThomasJonathan.Controllers
             {
                 db.DossierReservation.Add(dossierReservation);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("");
             }
 
             ViewBag.IdClient = new SelectList(db.Clients, "Id", "Mail", dossierReservation.IdClient);
