30ce9e935dcb2c092364d8859c47dca7ad73c11f
Thomas <Adminl@EMER06>
2018-07-30 20:48:26 +0200

    affichage voyages à venir sur le dashboard backoffice

--- BoVoyageBlandineThomasJonathan/Areas/BackOfficeConseiller/Controllers/DashboardController.cs
+++ BoVoyageBlandineThomasJonathan/Areas/BackOfficeConseiller/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoVoyageBlandineThomasJonathan.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,16 @@ namespace BoVoyageBlandineThomasJonathan.Areas.BackOfficeConseiller.Controllers
 {
     public class DashboardController : Controller
     {
+        private BoVoyageBTJDbContext db = new BoVoyageBTJDbContext();
         // GET: BackOfficeConseiller/Dashboard
         public ActionResult Index()
         {
-            return View();
+            
+
+            var today = DateTime.Now;
+            var quinze = today.AddDays(15);
+            var model = db.Voyages.Include("Destination").Where(x => x.DateAller > today && x.DateAller < quinze).OrderBy(x => x.DateAller).Take(5);
+            return View(model);
 
         }
 
--- BoVoyageBlandineThomasJonathan/Controllers/HomeController.cs
+++ BoVoyageBlandineThomasJonathan/Controllers/HomeController.cs
@@ -16,10 +16,13 @@ namespace BoVoyageBlandineThomasJonathan.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            //var quinze = intParse(DateTime).Now + 15;
-            var model = db.Voyages.Include("Destination").Where(x => x.DateAller > DateTime.Now).OrderBy(x => x.DateAller).Take(5);
+
+           
+
             
-            return View(model);
+
+
+            return View();
         }
 
         public ActionResult Contact()
